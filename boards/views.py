d1ee1e7d3a8a718d827256613a17dad821a68bdf
Steven Samy <stevensivo27@gmail.com>
2021-10-31 20:32:04 +0200

    create pages & URLs

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+# from typing_extensions import ParamSpecKwargs
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, response
 from .models import Board 
 
@@ -6,3 +7,12 @@ from .models import Board
 def home(request):
     boards = Board.objects.all()
     return render(request,'home.html', {'boards':boards})
+
+
+def board_topics(reqest,board_id):
+    board =get_object_or_404 (Board, pk=board_id)
+    return render(reqest,'topics.html',{'board':board})
+
+
+def about(reqest):
+    return HttpResponse(reqest,"yes")
