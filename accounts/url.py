aeae378588ba426eb854dd76e2fe8fc3ffb8b40c
Steven Samy <stevensivo27@gmail.com>
2021-11-11 22:09:29 +0200

    create login link

--- accounts/url.py
+++ accounts/url.py
@@ -3,5 +3,6 @@ from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path ('signup/',views.signup, name='signup' ),
-    path ('logout/', auth_views.LogoutView.as_view(), name='logout'  )
+    path ('logout/', auth_views.LogoutView.as_view(), name='logout'  ),
+    path ('login/', auth_views.LoginView.as_view(Template_name = 'login.html'), name='login')
 ]
