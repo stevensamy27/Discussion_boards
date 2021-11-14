57ab970e5d0c299a4a22fd8a54d746da45c0b4a5
Steven Samy <stevensivo27@gmail.com>
2021-11-14 16:09:40 +0200

    Topic Posts view

--- boards/views.py
+++ boards/views.py
@@ -23,7 +23,7 @@ def board_topics(request,board_id):
 def new_topic(request,board_id):
     board =get_object_or_404 (Board, pk=board_id)
     form = NewTopicForm()
-    # user = User.objects.first()
+    user = User.objects.first()
     if request.method == "POST":
         form = NewTopicForm(request.POST)
         if form.is_valid():
@@ -32,17 +32,27 @@ def new_topic(request,board_id):
             topic.created_by = request.user
             topic.save()
 
-            # post = Post.objects.create(
-            #     message = form.cleaned_data.get('message'),
-            #     created_by = user,
-            #     topic = topic
+            post = Post.objects.create(
+                message = form.cleaned_data.get('message'),
+                created_by = user,
+                topic = topic
 
-            # )
+            )
             return redirect('board_topics',board_id=board.pk)
 
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board':board, 'form':form})
 
+
+
+def topic_posts(request, board_id, topic_id):
+    topic = get_object_or_404(Topic, board__pk=board_id,pk=topic_id)
+
+
+    return render(request,'topic_posts.html',{'topic':topic})
+
+
 def about(request):
+
     return HttpResponse(request,"yes")
