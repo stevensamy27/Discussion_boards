84f0a87ce75362b8585ec514f99c91e89124be5f
Steven Samy <stevensivo27@gmail.com>
2021-11-14 14:14:33 +0200

    Protecting views

--- boards/views.py
+++ boards/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, response
 from .models import Board, Topic
 from django.contrib.auth.models import  User
 from.models import Topic,Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -17,16 +18,18 @@ def board_topics(request,board_id):
     board =get_object_or_404 (Board, pk=board_id)
     return render(request,'topics.html',{'board':board})
 
+
+@login_required
 def new_topic(request,board_id):
     board =get_object_or_404 (Board, pk=board_id)
     form = NewTopicForm()
-    user = User.objects.first()
+    # user = User.objects.first()
     if request.method == "POST":
         form = NewTopicForm(request.POST)
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.created_by = user
+            topic.created_by = request.user
             topic.save()
 
             # post = Post.objects.create(
